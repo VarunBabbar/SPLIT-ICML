cedc434b1bd25187989cf8945362d9ce65be6235
VarunBabbar <vb97@duke.edu>
2026-03-21 15:13:07 -0400

    Added multi-class error message for SPLIT and LicketySPLIT

--- split/src/split/LicketySPLIT.py
+++ split/src/split/LicketySPLIT.py
@@ -64,6 +64,8 @@ class LicketySPLIT:
         (in the latter case X_train is binarized according to the threshold
         guessing transform, as fit on X_train)
         '''
+        if len(np.unique(y_train)) > 2:
+            raise ValueError("LicketySPLIT only supports binary classification for now.")
         if self.binarize:
             X_train_bin = self.enc.fit_transform(X_train, y_train)
         else:
